5636dbb872e842eb110dd64fbd09021dc53fc6c7
jamshednita <32198248+jamshednita@users.noreply.github.com>
2018-05-08 23:08:45 +0530

    sort hashmap on values

--- src/com/corejava/MainApp.java
+++ src/com/corejava/MainApp.java
@@ -1,9 +1,16 @@
 package com.corejava;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Map.Entry;
 
 public class MainApp {
 
@@ -26,6 +33,28 @@ public class MainApp {
 		System.out.println(emps.size());
 		
 		System.out.println(throwEx());
+		
+		// Sort a map based on values ==
+		Map<String, Integer> map = new HashMap<String, Integer>();
+        map.put("java", 20);
+        map.put("C++", 45);
+        map.put("Java2Novice", 2);
+        map.put("Unix", 67);
+        map.put("MAC", 26);
+        map.put("Why this kolavari", 93);
+        
+        Set<Entry<String, Integer>> set = map.entrySet();
+        List<Entry<String, Integer>> list = new ArrayList<>(set);
+        Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> arg0, Entry<String, Integer> arg1) {
+				// Reverse order
+				return arg1.getValue().compareTo(arg0.getValue());
+			}
+		});
+        
+        list.forEach((e)->{System.out.println(e.getKey() + " :: " + e.getValue());});
 	}
 	
 	static int throwEx(){
